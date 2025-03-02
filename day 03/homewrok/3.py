caef54f12340ea7bf98697e428c8ce7897ac95a1
vazha-mgeli <vazhamgeladze61@gmail.com>
2025-03-02 16:28:46 +0400

    first commit

---
+++ day 03/homewrok/3.py
@@ -0,0 +1,11 @@
+wolf1 = "teenparrot"
+wolf2 = "parrot"
+wolk3 = "teenwolf"
+
+wolf4 = "423"
+wolf5 = "786"
+wolf6 = "396"
+
+wolf7 = "2.56"
+wolf8 = "476"
+wolf9 = "7.90"
---
+++ day 06/homework/6.py
@@ -0,0 +1,4 @@
+name = "vazha"
+surname = "mgeladze"
+age = 11
+Date_of_birth = 2013_12_21
