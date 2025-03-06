0633a6af7827072c6f6d89cc098f90106e6cb93d
vazha-mgeli <vazhamgeladze61@gmail.com>
2025-03-06 16:51:01 +0400

    second commit

---
+++ day 08/classwork/cw.py
@@ -0,0 +1,21 @@
+# type- ფუნქცია გამოიყენება იმისთვის, რომ დაადგინოთ ობიექტის ტიპი. როდესაც ამ ფუნქციას გადასცემთ ობიექტს, ის დაგიბრუნებთ ობიექტის კლასს.
+
+var1 = 10           # integer
+var2 = 3.14         # float
+var3 = "Python"     # string
+var4 = True         # boolean
+var5 = [1, 2, 3]    # list
+
+# მონაცემთა ტიპების შემოწმება
+print(type(var1))  # int
+print(type(var2))  # float
+print(type(var3))  # str
+print(type(var4))  # bool
+print(type(var5))  # list
+
+
+num1 = 10
+num2 = 5
+
+print("num1 - num2")
+print("num1 + num2")
---
+++ day 08/homework/hw 2.py
@@ -0,0 +1,6 @@
+messi = "ვაჟა"
+ronaldo = "არის"
+neymar = "ნომერ"
+yamal = "პირველი ფეხბურთელი"
+
+print(messi, ronaldo, neymar, yamal)
